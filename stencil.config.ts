6715857473c1fe4339a22b75ecf303b32ed9305e
mr-steal-ur-code <cjkarkut@gmail.com>
2023-04-24 14:05:29 -0500

    move global css and ts to root

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,8 +3,8 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: 'src/global/app.css',
-  globalScript: 'src/global/app.ts',
+  globalStyle: 'src/app.css',
+  globalScript: 'src/app.ts',
   taskQueue: 'async',
   outputTargets: [
     {
