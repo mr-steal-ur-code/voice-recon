cc9500fa152eddb8273190d86fdb1844e29c61e6
mr-steal-ur-code <cjkarkut@gmail.com>
2023-04-24 13:40:30 -0500

    add firestore

---
+++ src/components/app-tabs/env/live/src/config.ts
@@ -0,0 +1,13 @@
+export default {
+  env: 'live',
+  url: 'https://',
+  firebase: {
+    apiKey: 'AIzaSyCAA7Om2X_KRlSXrHZ3_pTZbxGLvXx4lYc',
+    authDomain: 'voice-recon-d7491.firebaseapp.com',
+    projectId: 'voice-recon-d7491',
+    storageBucket: 'voice-recon-d7491.appspot.com',
+    messagingSenderId: '643991680510',
+    appId: '1:643991680510:web:a5fd2e68731a488dd0bcaf',
+    measurementId: 'G-0RM2DTWGNP',
+  },
+};
