4046d65bf0e96531a3aba816250a9000538cd0e8
mr-steal-ur-code <cjkarkut@gmail.com>
2023-04-24 18:23:01 -0500

    set heights on divs

--- src/components/maps/rpd/east/rpd-east.tsx
+++ src/components/maps/rpd/east/rpd-east.tsx
@@ -7,8 +7,18 @@ import { Component, h } from '@stencil/core';
 export class RpdEast {
   render() {
     return (
-      <div style={{ width: '100%', height: '100%', background: '#857373' }}>
-        <div class="clock" style={{ height: '30%', width: '30%' }}>
+      <div style={{ display: 'absolute', width: '100%', height: '100%', background: '#857373' }}>
+        <div
+          class="clock"
+          style={{
+            height: '30%',
+            width: '30%',
+            background: 'red',
+            border: '1px solid black',
+            top: 'calc(100% - 35%)',
+            left: 'calc(100% - 35%)',
+          }}
+        >
           <span style={{ height: '50px', width: '5px' }}></span>
         </div>
       </div>
